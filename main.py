53e89a7f3e77ad35bd92285aa97cf6eaf2642092
shreyon312 <52107043+shreyon312@users.noreply.github.com>
2021-02-02 23:23:27 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+from Bio.Graphics import GenomeDiagram
+from Bio import SeqIO
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+
+record = SeqIO.read("Genome.gb", "genbank")
+
+gd_diagram = GenomeDiagram.Diagram(record.id)
+gd_track_for_features = gd_diagram.new_track(1, name="Annotated Features")
+gd_feature_set = gd_track_for_features.new_set()
+
+for feature in record.features:
+    if feature.type != "gene":
+        #Exclude this feature
+        continue
+    if len(gd_feature_set) % 2 == 0:
+        color = colors.green
+    else:
+        color = colors.red
+    gd_feature_set.add_feature(feature, sigil="ARROW",
+                               color=color, label=True,
+                               label_size = 14, label_angle=0)
+gd_diagram.draw(format="circular", circular=True, pagesize=(20*cm,20*cm),
+                start=0, end=len(record), circle_core=0.7)
+gd_diagram.write("genome_circular.pdf", "PDF")
+gd_diagram.write("genome_circular.png", "PNG")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
